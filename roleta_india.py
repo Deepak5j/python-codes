18cafe8c9efa415f8ab38dc9fcd6c891af119e99
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2022-10-27 17:15:52 +0530

    :100: :+1:

---
+++ roleta_india.py
@@ -0,0 +1,17 @@
+import random
+import os
+
+# single bullet loaded gun
+gun = [0,0,0,0,1,0] 
+
+while gun:
+    path = input("Enter you OS system path: ")
+    bullet = random.choice(gun)
+    if bullet == 1:
+        print("you dead")
+        #os.remove(path)
+        break
+    gun.remove(bullet)
+    
+        
+    
